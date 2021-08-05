462f8d7bd23834255312d0e6f533f500c89842bd
Tan (CaivanPC) <38083823+tanho63@users.noreply.github.com>
2021-08-05 12:50:44 -0400

    update

--- tweets_32beatwriters.R
+++ tweets_32beatwriters.R
@@ -5,19 +5,19 @@ library(purrr)
 
 twitter_token <- rtweet::create_token(
   app = "Tan-DataCollection",
-  consumer_key = Sys.getenv("TwitterAPIKey"),
-  consumer_secret = Sys.getenv("TwitterAPISecret"),
-  access_token = Sys.getenv("TwitterAccessToken"),
-  access_secret = Sys.getenv("TwitterAccessTokenSecret")
+  consumer_key = Sys.getenv("TWITTERAPIKEY"),
+  consumer_secret = Sys.getenv("TWITTERAPISECRET"),
+  access_token = Sys.getenv("TWITTERACCESSTOKEN"),
+  access_secret = Sys.getenv("TWITTERACCESSTOKENSECRET")
 )
 
 min_id <- arrow::open_dataset("data/32beatwriters", format = "csv") %>% 
   select(status_id) %>% 
   collect() %>% 
   filter(status_id == max(status_id)) %>% 
-  pull()
+  pull(status_id)
 
-tweets_32 <- rtweet::get_timelines(user = "32beatwriters", 
+tweets_32 <- rtweet::get_timeline(user = "32beatwriters", 
                                    n = 3200,
                                    token = twitter_token) %>% 
   filter(status_id >= min_id) %>% 
